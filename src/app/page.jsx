376ddc0a4b474dc28f7a9fa7272c78bdad53102f
KeYi <46948667+bean4896@users.noreply.github.com>
2023-11-03 07:02:01 +0800

    name

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,11 +18,11 @@ import logoUnseal from '@/images/clients/unseal/logo-light.svg'
 import imageLaptop from '@/images/laptop.jpg'
 
 import logoWhiteMyseat from '@/images/clients/myseat/logo-white-myseat-3x.png'
-import logoWhiteLCS from '@/images/clients/laochangsha/logolcswhite.png'
+import logoWhiteLCS from '@/images/clients/laochangsha/logo-lcs-white.png'
 import logoLokyo from '@/images/clients/lokyo/lokyo-white.png'
 import logoJuanXinCai from '@/images/clients/juanxincai/juanxincai.png'
 import logoGeMeiNa from '@/images/clients/gemeina/gemeina-white.png'
-import logoFreePath from '@/images/clients/freepath/freepathwhite.png'
+import logoFreePath from '@/images/clients/freepath/logo-freepath.png'
 import { loadMDXMetadata } from '@/lib/loadMDXMetadata'
 
 const clients = [
